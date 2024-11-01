d345cf64c91edd0b1eb1431dafe1e9c6a208beca
Oskar Przybylski <155388816+oskar-przybylski@users.noreply.github.com>
2024-11-01 19:59:00 +0100

    Zmiany kosmetyczne
    dodanie kolorow przy komunikatach itp

--- isod_api.py
+++ isod_api.py
@@ -6,14 +6,16 @@ import time
 base_url = "http://isod.ee.pw.edu.pl/isod-portal/wapi"
 params = {
     "q": "mynewsheaders",
-    "username": " :D", #wstaw login z isoda: "imie.nazwisko"
-    "apikey": " :D",   #wstaw klucz api: "XXXXXXXXXX"
+    "username": " :D  ", #wstaw login z isoda: "imie.nazwisko"
+    "apikey": "  :D  ",   #wstaw klucz api: "XXXXXXXXXX"
     "from": "0",
     "to": "5",
 }
 
-max_retries = 5
-delay = 3
+max_retries = 2
+delay = 2
+
+init();
 
 start_time = time.perf_counter()
 for attempt in range(max_retries):
@@ -23,13 +25,14 @@ for attempt in range(max_retries):
         data = response.json()
         break
     except requests.exceptions.ConnectionError:
-        print(f"Próba polaczenia z serwerem isod nr.{attempt + 1} nieudana. Ponawiam próbę za {delay} sekund...")
+        print(Style.DIM+Fore.RED+"[?]"+Style.RESET_ALL,Style.DIM,Style.DIM+f"Próba polaczenia z serwerem isod nr.{attempt + 1} nieudana. Ponawiam próbę za {delay} sekund..."+Style.RESET_ALL)
         time.sleep(delay)
     except requests.exceptions.HTTPError as err:
         print("Błąd HTTP:", err)
         break
 else:
-    print("Nie udało się nawiązać połączenia po maksymalnej liczbie prób.")
+    print(Style.DIM+Fore.RED+"[-]"+Style.RESET_ALL,Style.DIM+"Nie udało się nawiązać połączenia po maksymalnej liczbie prób."+Style.RESET_ALL)
+    exit()
 end_time = time.perf_counter()
 
 request_duration = end_time - start_time
@@ -39,13 +42,12 @@ if response.status_code == 200:
 else:
     print("Błąd: ", response.status_code)
 
-init();
 print("\n");
 print("─" * 44,Style.BRIGHT+Fore.YELLOW+"ISOD"+Style.RESET_ALL,"─" * 50,)
 for item in data["items"]:
     if(item["type"]!=1000):
         if(item["type"]==1002):
-            print(Fore.RED+item["subject"]+Style.RESET_ALL,"by ",item["modifiedBy"])
+            print(Style.BRIGHT+Fore.GREEN+"[+]"+Style.RESET_ALL,Fore.RED+item["subject"]+Style.RESET_ALL,"by ",item["modifiedBy"])
             print("─" * 100)
         else:
             print(item["subject"],"by ",item["modifiedBy"])
